c0503e802f099c851cc1448534936b2c3984df72
Aya <benmakhloufaya1@gmail.com>
2023-09-03 03:21:39 +0200

    Début du codage du jeu

---
+++ _js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+const WORDLIST = ["soleil", "banane", "orange", "fraise", "maison"];
+const MAXATTEMPTS = 10;
+let attempts = 0;
+var randomWord;
+
+window.addEventListener("DOMContentLoaded", function() {
+let letterColumn = document.querySelectorAll('.letter-column');
+// let guessForm = document.querySelector('.guess-form');
+let guessButton = document.querySelector('#guess-button')
+let guessInput = document.querySelector('#guess-input');
+let message = document.querySelector('.msg');
+
+
+let currentGuess = guessInput.value; 
+
+
+
+}
+);
